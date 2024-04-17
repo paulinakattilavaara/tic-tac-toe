e6983e29539c43fbf6c1380affa15cf56caf2f63
Paulina Kattilavaara <paulinakatt@hotmail.com>
2024-04-17 07:54:05 +0200

    fixed the font for the squares

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -6,7 +6,7 @@ const Board = () => {
         <div className="row1"><Square id={1} /><Square id={2} /><Square id={3} /></div>
         <div className="row2"><Square id={4} /><Square id={5} /><Square id={6} /></div>
         <div className="row3"><Square id={7} /><Square id={8} /><Square id={9} /></div>
-        <button className="restart">Restart game</button>
+        <button className="restart">Start game</button>
     </div>
   )
 }
