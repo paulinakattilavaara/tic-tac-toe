1a6d6e60b1c2d7e9a9f6c1fc79220613d9c83dda
Paulina Kattilavaara <paulinakatt@hotmail.com>
2024-04-18 14:38:45 +0200

    added some text shadow and a little alert with instructions

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 import { GiTicTacToe } from "react-icons/gi";
 
 const iconClick = () => {
-  console.log("Klickade på ikonen.")
+  alert("\n✖️TIC TAC TOE✖️\n\nClick on the squares on the game board to place your mark. ❌⭕\n\nThe first player to get three in a row either horizontally, vertically, or diagonally wins! 🏆\n\nIf all squares are filled without three in a row, the game ends in a draw. ⚖️\n\nTo play again click on the 'Restart' button!🔁")
 }
 
 const Header = () => {
@@ -12,4 +12,4 @@ const Header = () => {
   )
 }
 
-export default Header
+export default Header;
